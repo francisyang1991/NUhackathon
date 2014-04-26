d194a4515f274f720e49b1888802c414d1fd583d
xytan123 <xytan123@gmail.com>
2014-04-25 23:57:12 -0500

    xiaoyang
    get json

--- index.js
+++ index.js
@@ -1,3 +1,7 @@
+var terms;
+var schools;
+var subjects;
+var url_head = 'http://vazzak2.ci.northwestern.edu/';
 $(document).ready(function () {
 
 	     // $(":radio").change(function(){
@@ -5,4 +9,21 @@ $(document).ready(function () {
 	     // 	console.log(checked);
 	     // });
 
+	terms = $.getJSON(url_head+'terms', function(data) {
+	             terms=data;
+	          });
+	console.log(terms);
+	schools = $.getJSON(url_head+'schools', function(data) {
+	             schools=data;
+	          });
+	console.log(schools);
+
+	subjects = $.getJSON(url_head+'subjects', function(data) {
+             subjects=data;
+          });
+	console.log(subjects);
+
 })
+
+
+
