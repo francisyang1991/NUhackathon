f2c0de0222de2a5bbacc0c799b06f46f1c562953
Zhou Yuchao <513629287@qq.com>
2014-04-26 00:17:46 -0500

    nothing

--- packaged/javascript/getData.js
+++ packaged/javascript/getData.js
@@ -1,32 +1,22 @@
-
-function getSchool(result)
-{
-  alert(result);
-}
-
 function querySchool()
 {
-  alert("querySchool");
-  var JSONP=document.createElement("script");  
-  JSONP.type="text/javascript";  
-  JSONP.src="http://vazzak2.ci.northwestern.edu/schools";  
-  document.getElementsByTagName("head")[0].appendChild(JSONP);  
-  alert(JSONP[0].symbol);
-  alert(JSONP[0].name);
-
+    $.getJSON('http://vazzak2.ci.northwestern.edu/schools/', function(result) {  
+      console.log(result);
+    });
 }
 
 function queryTerms()
 {
-  alert("queryTerms11111");
-  var JSONP=document.createElement("script");  
-  JSONP.type="text/javascript";  
-  JSONP.src="http://vazzak2.ci.northwestern.edu/terms";  
-  document.getElementsByTagName("head")[0].appendChild(JSONP);  
-  //alert(JSONP[0].symbol);
-  alert(JSONP[0].name);
-  //console.log(JSONP[0].symbol);
-  //console.log(JSONP[0].name);
-  alert("finish");
+  $.getJSON('http://vazzak2.ci.northwestern.edu/terms/', function(result) {  
+      console.log(result);
+    });
 }
+
+function querySubjects()
+{
+  $.getJSON('http://vazzak2.ci.northwestern.edu/subjects/', function(result) {  
+      console.log(result);
+    });
+}
+
 
